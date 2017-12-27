d9e9bb6e1016496c4f08e70cecee1cc0159f9ac7
Murat Tunaboylu <murattunaboylu@gmail.com>
2017-12-27 13:06:06 +0100

    Exporting aggregated OHLC data

---
+++ Freedom/Freedom.SimulatorServices/Controllers/ExportController.cs
@@ -0,0 +1,131 @@
+﻿using Freedom.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Freedom.SimulatorServices.Controllers
+{
+
+    public class ExportController : ApiController
+    {
+        [EnableCors("*", "*", "*")]
+        public IHttpActionResult Get(string start, string end, int interval, [FromUri] StrategyParameters parameters)
+        {
+            var startDate = DateTime.ParseExact(start, "yyyyMMdd", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(end, "yyyyMMdd", CultureInfo.InvariantCulture);
+            var intervalInMinutes = interval;
+
+            var exportLines = Export(startDate, endDate, intervalInMinutes, parameters);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+
+            result.Content = new StringContent(exportLines.First());
+
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = $"marketdata.export.{DateTime.Now:yyyMMddHHmmss}.csv"
+            };
+
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            var response = ResponseMessage(result);
+
+            return response;
+        }
+
+        public List<string> Export(DateTime start, DateTime end, int interval, StrategyParameters parameters)
+        {
+            //Read the OHLC from database
+            var connectionString = ConfigurationManager.ConnectionStrings["marketdata-local"].ConnectionString;
+
+            List<OHLC> ohlcList = new List<OHLC>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("", connection))
+                    {
+                        command.CommandType = System.Data.CommandType.Text;
+                        command.CommandText = "SELECT o.Id, o.[Open], o.High, o.Low, o.[Close], Start, Volume FROM dbo.OHLC o " +
+                            "WHERE o.[Start] >= @start AND o.[End] < @end ORDER BY o.Id ASC";
+                        command.Parameters.Add(new SqlParameter("start", start));
+                        command.Parameters.Add(new SqlParameter("end", end));
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var ohlc = new OHLC();
+
+                                ohlc.Open = reader.GetDecimal(1);
+                                ohlc.High = reader.GetDecimal(2);
+                                ohlc.Low = reader.GetDecimal(3);
+                                ohlc.Close = reader.GetDecimal(4);
+                                ohlc.Start = reader.GetDateTime(5);
+                                var volume = reader.GetDecimal(6);
+                                ohlc.Volume = (double)(volume * 1000);
+
+                                ohlcList.Add(ohlc);
+                            }
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            //l h o c
+
+            //Find the first trade
+            //Find all trades within the next 5 minutes
+            //If there are any trade
+            //Calculate low, high, open, close
+            if (ohlcList.Any())
+            {
+                start = ohlcList.First().Start > start ? ohlcList.First().Start : start;
+
+                for (DateTime i = start; i < end; i = i.AddMinutes(interval))
+                {
+                    var windowStart = i;
+                    var windowEnd = windowStart.AddMinutes(interval);
+
+                    var ohlcInTheSameWindow = ohlcList.Where(o => o.Start >= windowStart && o.Start < windowEnd).ToList();
+
+                    if (ohlcInTheSameWindow.Any())
+                    {
+                        var low = ohlcInTheSameWindow.Select(t => t.Low).Min();
+                        var high = ohlcInTheSameWindow.Select(t => t.High).Max();
+                        var open = ohlcInTheSameWindow.First().Open;
+                        var close = ohlcInTheSameWindow.Last().Close;
+                        var volume = ohlcInTheSameWindow.Sum(t => t.Volume);
+
+                        var ohlc = new OHLC(open, high, low, close) { Volume = volume, Start = windowStart, End = windowEnd };
+
+                        DataPoints.Add(ohlc);
+                    }
+                }
+            }
+
+            var exportLines = DataPoints.Select(d => $"{d.Start},{d.Close}");
+
+            return exportLines.ToList();
+        }
+
+        public List<OHLC> DataPoints { get; set; } = new List<OHLC>();
+    }
+}
